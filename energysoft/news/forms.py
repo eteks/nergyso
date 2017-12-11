3db7339fd75236ad2a5912961eb6d03bc45434ce
anand <anand@etekchnoservices.com>
2017-12-11 12:27:07 +0530

    21_11_12.25(validator and forms for news)

---
+++ energysoft/news/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class FileFieldForm(forms.ModelForm):
+    news_document = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+    def clean_image(self):
+         news_image = self.cleaned_data.get('news_image',False)
+         print 'imgsize_'+ news_image._size
+         if news_image:
+             if news_image._size > 4*1024*1024:
+                   raise ValidationError("Image file too large ( > 4mb )")
+             return news_image
+         else:
+             raise ValidationError("Couldn't read uploaded image")
