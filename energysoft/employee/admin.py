0d580c2cce0d02f91347464d3d4518ff5a4fa72e
KalaiR <kalaimca.gs@gmail.com>
2018-01-12 15:19:28 +0530

    update employee active status

--- energysoft/employee/admin.py
+++ energysoft/employee/admin.py
@@ -13,7 +13,7 @@ class EmployeeAdmin(admin.ModelAdmin):
 	form= EmpFileForm
 	#updated by kalai
 
-	exclude = ('password','is_superuser','is_active','last_login','date_joined','groups','user_permissions','employee_aadhar_id','is_staff','active_status','delete_status','employee_device_id')
+	exclude = ('password','is_superuser','is_active','last_login','date_joined','groups','user_permissions','employee_aadhar_id','is_staff','employee_device_id')
 	# exclude = ('password','first_name','last_name','is_superuser','is_active','last_login','date_joined','groups','user_permissions','email','employee_aadhar_id','is_staff','employee_device_id')
 
 	list_display = ('employee_id','employee_name','employee_mobile','employee_designation')
--- energysoft/energysoft/settings.py
+++ energysoft/energysoft/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vdart', 
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
