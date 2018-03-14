77ad0681f3f1c91fb0a4fa6927334bbf637c0ecd
KalaiR <kalaimca.gs@gmail.com>
2018-03-14 12:39:09 +0530

    update post save news

--- energysoft/news/models.py
+++ energysoft/news/models.py
@@ -77,10 +77,31 @@ def save_news(sender, instance, **kwargs):
 			print "notification_id"
 			print note.id
 			devices = GCMDevice.objects.get(user_id=emp_id)
+			reg_id =  devices.registration_id
+			print "reg_id"
+			print reg_id
 			devices.send_message(instance.news_title, title="News posted",extra={"news_id": instance.id,"category":"news","notification_id":note.id})
 		# devices = GCMDevice.objects.all()
 		# for q in devices:
 		# 	q.send_message(instance.news_title, title="News posted",extra={"news_id": instance.id,"category":"news"})
+		# print(p)
+
+# def save_news(sender, instance, **kwargs):
+# 	emp_id = Employee.objects.all().values_list('user_ptr_id', flat=True)
+# 	# print(emp_id)
+# 	cateogry = "News"
+# 	cateogry_id=Notification.objects.filter(notification_cateogry=cateogry,notification_cateogry_id=instance.id).exists()
+# 	# print event_id
+# 	if cateogry_id:   
+# 		pass
+# 	else:
+# 		for p in emp_id:
+# 			note = Notification(notification_cateogry=cateogry,notification_cateogry_id=instance.id,notification_delivery_status=0,notification_read_status=0,notification_created_date=instance.created_date,notification_employee_id=p)
+# 			# print note
+# 			note.save()
+# 		devices = GCMDevice.objects.all()
+# 		for q in devices:
+# 			q.send_message(instance.news_title, title="News posted",extra={"news_id": instance.id,"category":"news"})
 		# print(p)
 
 post_save.connect(save_news, sender=News)
